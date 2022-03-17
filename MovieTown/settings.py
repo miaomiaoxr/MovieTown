a6f7745d9ed5a145d214b983a6455ed21f616097
Xinhao Yi <2627827Y@student.gla.ac.uk>
2022-03-17 15:15:43 +0000

    yxh z 2 to 1

--- MovieTown/settings.py
+++ MovieTown/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,7 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR,]
-
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 # User auth config
 LOGIN_URL = 'auth_login'
--- movie/views.py
+++ movie/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse,Http404,JsonResponse
 from django.shortcuts import redirect
 from movie.models import Comment
 from django.contrib.auth.decorators import login_required
+from movie.models import Category
+from movie.models import Movie
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage, InvalidPage
 
 # Create your views here.
 @login_required
@@ -35,4 +38,85 @@ def user_profile(request):
 
 
 def index(request):
-    return render(request,'movie/index.html')
+    # yxh test, can be deleted
+    category_list = Category.objects.all()
+    context_dict = {'categories': category_list}
+    return render(request,'movie/index.html', context = context_dict)
+    # return render(request,'movie/index.html')
+    
+    
+    
+def show_category(request, category_name_slug):
+    # Create a contextual dictionary to be passed to the template rendering engine later
+    context_dict = {}
+    
+    try:
+        category_list = Category.objects.all()
+        
+        # Find the corresponding category by passing in the category alias
+        category = Category.objects.get(slug = category_name_slug)
+    
+        # Find all movies with this category by foreign key
+        movies = Movie.objects.filter(category=category)
+        
+        # Display 1 item per page as specified, split
+        paginator = Paginator(movies, 12) 
+        
+        
+        page = request.GET.get('page', default = '1')
+        
+        try:
+            movies = paginator.page(page)
+        except PageNotAnInteger:
+            # If the number of pages requested is not an integer, the first page is returned.
+            books = paginator.page(1)
+        except InvalidPage:
+            # If the requested page does not exist, redirect the page
+            return HttpResponse('The content of the page cannot be found')
+        except EmptyPage:
+            # If the requested page is not within the legal page count, the last page of the result is returned.
+            books = paginator.page(paginator.num_pages)
+            
+        context_dict['categories'] = category_list
+        context_dict['category'] = category
+        context_dict['movies'] = movies
+        
+    except Category.DoesNotExist:
+        context_dict['categories'] = None
+        context_dict['category'] = None
+        context_dict['movies'] = None
+        
+        
+    return render(request, 'movie/category.html', context = context_dict)
+
+
+def moviepage(request,movie_name_slug):
+    # Create a context dictionary which we can pass
+    # to the template rendering engine.
+    context_dict = {}
+    try:
+        # Can we find a category name slug with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception.
+        # The .get() method returns one model instance or raises an exception.
+        category = Movie.objects.get(slug=movie_name_slug)
+        comments= Comment.objects.filter(movie=category)
+        like_count=Comment.objects.filter(movie=category,liked_flag=True).count()
+        # Retrieve all of the associated pages.
+        # The filter() will return a list of page objects or an empty list.
+        # Adds our results list to the template context under name pages.
+        # We also add the category object from
+        # the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.
+        context_dict['movie'] = category
+        context_dict['comments'] =comments
+        context_dict['count'] = like_count
+    except Movie.DoesNotExist:
+        # We get here if we didn't find the specified category.
+        # Don't do anything -
+        # the template will display the "no category" message for us.
+        context_dict['movie'] = None
+        # Go render the response and return it to the client.
+    return render(request, 'movie/movie.html', context=context_dict)
+
+        
+    

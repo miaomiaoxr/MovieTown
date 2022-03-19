356fc6621a6be36d016afeb39bde384cae2b8a29
miaomiaoxr <miaomiaoxr1234@gmail.com>
2022-03-19 17:59:48 +0000

    Finish all work

--- movie/urls.py
+++ movie/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 app_name = 'movie'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.homepage, name='index'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
     path('<slug:movie_name_slug>/', views.moviepage, name ='moviepage'),
     path('submit/<slug:movie_name_slug>/', views.submit_func),
